62ef4ea691a297d063234abba4cc7786f3a41a65
John Tanner <j.d.tanner@gmail.com>
2012-06-08 21:57:48 +0100

    Created main tracking algorithm
    peaksky_rtty is now no longer updated. All code will be brought into
    peaksky_gps instead, which has been tidied up and debug comments
    removed.

--- peaksky_gps/peaksky_gps.ino
+++ peaksky_gps/peaksky_gps.ino
@@ -1,6 +1,14 @@
+//Include header files required for sketch
 #include <SoftwareSerial.h>
 #include <TinyGPS.h>
 #include <util/crc16.h>
+
+//Required for RTTY function
+#define RADIO_SPACE_PIN 10
+#define RADIO_MARK_PIN 11
+#define ASCII 7
+#define BAUD 50
+#define INTER_BIT_DELAY (1000/BAUD)
 
 //Define some variables to hold GPS data
 unsigned long date, time, age;
@@ -18,35 +26,43 @@ SoftwareSerial ss(2,3);
 //Setup function of Arduino
 void setup() {
 
+  //Set up RTTY pins
+  pinMode(RADIO_MARK_PIN,OUTPUT);
+  pinMode(RADIO_SPACE_PIN,OUTPUT);
+
   //Start up software and hardware serial at 9600 baud
   ss.begin(9600);
   Serial.begin(9600);
   delay(2000);
 
   //Set the GPS into airborne mode
-  Serial.print("Setting uBlox nav mode: ");
   uint8_t setNav[] = {
-    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC        };
+    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC          };
   sendUBX(setNav, sizeof(setNav)/sizeof(uint8_t));
   getUBX_ACK(setNav);
 
   //Disable GPS data that we don't need
-  Serial.println("Disabling GPS commands");
   ss.print("$PUBX,40,GLL,0,0,0,0*5C\r\n");
+  delay(500);
   ss.print("$PUBX,40,ZDA,0,0,0,0*44\r\n");
+  delay(500);
   ss.print("$PUBX,40,VTG,0,0,0,0*5E\r\n");
+  delay(500);
   ss.print("$PUBX,40,GSV,0,0,0,0*59\r\n");
+  delay(500);
   ss.print("$PUBX,40,GSA,0,0,0,0*4E\r\n");
+  delay(500);
   ss.print("$PUBX,40,RMC,0,0,0,0*47\r\n");
+  delay(500);
   ss.print("$PUBX,40,GGA,0,0,0,0*5A\r\n");
-
+  delay(500);
+  
   //Give the GPS time to breathe :)
-  delay(5000);
+  delay(2500);
 }
 
 //Loop function of Arduino
 void loop() {
-  //Serial.println("Requesting NMEA sentence from GPS");
 
   //Request NMEA sentence from GPS
   ss.print("$PUBX,00*33\r\n");
@@ -62,16 +78,9 @@ void loop() {
 
     //Only if TinyGPS has received a complete NMEA sentence
     if (checkNMEASentence > 0) {
-      //Serial.println("TinyGPS received a complete NMEA sentence");
-
-      //Print interation count of loop
-      //Serial.print("Interation: ");
-      //Serial.println(iteration);
 
       //Query the TinyGPS object for the number of satellites
       numberOfSatellites = gps.sats();
-      //Serial.print("Number of satellites: ");
-      //Serial.println(numberOfSatellites);
 
       //Query the TinyGPS object for the date, time and age
       gps.get_datetime(&date, &time, &age);
@@ -81,9 +90,6 @@ void loop() {
       minute = ((time - (hour * 1000000)) / 10000);
       second = ((time - ((hour * 1000000) + (minute * 10000))));
       second = second / 100;
-
-      //Output the time data to a character array
-      //sprintf(timeBuffer, "%02d:%02d:%02d", hour, minute, second);
 
       if (numberOfSatellites >= 1) {
 
@@ -103,123 +109,117 @@ void loop() {
         //Convert altitude to metres
         gpsAltitude = (gps.altitude() / 100);
 
-        //Serial.print("Latitude: ");
-        //Serial.println(latitudeBuffer);
-        //Serial.print("Longitude: ");
-        //Serial.println(longitudeBuffer);
-        //Serial.print("Altitude: ");
-        //Serial.println(gpsAltitude);    
-
+        //Construct the transmit buffer
         transmitCheck=sprintf(transmitBuffer, "$$PEAKSKY,%d,%02d:%02d:%02d,%s,%s,%ld,%d", iteration, hour, minute, second, latitudeBuffer, longitudeBuffer, gpsAltitude, numberOfSatellites);
 
         if (transmitCheck > -1){
+
+          //Append the CRC16 checksum to the end of the transmit buffer
           transmitCheck = sprintf (transmitBuffer, "%s*%04X\n", transmitBuffer, gps_CRC16_checksum(transmitBuffer));
+
+          //Pass the transmit buffer to the RTTY function
           Serial.print(transmitBuffer);
-          //rtty_txstring(superbuffer);
+          rtty_txstring(transmitBuffer);
+
+          // Delay for 8.5 seconds
+          delay(8500);                                     
+
         }
         iteration++;        
       }
-
-      //Serial.print("Time: ");
-      //Serial.println(timeBuffer);
-      //Serial.println("------------------------");
     }
   }
-
-  //This creates a 10 second gap between GPS readouts
-  delay(10000);
 }
 
 ////////////////////
 //HELPER FUNCTIONS//
 ////////////////////
 
-// Send a byte array of UBX protocol to the GPS
-void sendUBX(uint8_t *MSG, uint8_t len) {
+void sendUBX(uint8_t *MSG, uint8_t len) {           // Send a byte array of UBX protocol to the GPS
   for(int i=0; i<len; i++) {
     ss.write(MSG[i]);
-    Serial.print(MSG[i], HEX);
   }
-  Serial.println();
 }
 
-// Calculate expected UBX ACK packet and parse UBX response from GPS
-boolean getUBX_ACK(uint8_t *MSG) {
+boolean getUBX_ACK(uint8_t *MSG) {                  // Calculate expected UBX ACK packet and parse UBX response from GPS
   uint8_t b;
   uint8_t ackByteID = 0;
   uint8_t ackPacket[10];
   unsigned long startTime = millis();
-  Serial.print(" * Reading ACK response: ");
-
-  // Construct the expected ACK packet    
-  ackPacket[0] = 0xB5;	        // header
-  ackPacket[1] = 0x62;	        // header
-  ackPacket[2] = 0x05;	        // class
-  ackPacket[3] = 0x01;	        // id
-  ackPacket[4] = 0x02;	        // length
+                                                    // Construct the expected ACK packet    
+  ackPacket[0] = 0xB5;	                            // header
+  ackPacket[1] = 0x62;	                            // header
+  ackPacket[2] = 0x05;	                            // class
+  ackPacket[3] = 0x01;	                            // id
+  ackPacket[4] = 0x02;	                            // length
   ackPacket[5] = 0x00;
-  ackPacket[6] = MSG[2];	// ACK class
-  ackPacket[7] = MSG[3];	// ACK id
-  ackPacket[8] = 0;		// CK_A
-  ackPacket[9] = 0;		// CK_B
+  ackPacket[6] = MSG[2];	                    // ACK class
+  ackPacket[7] = MSG[3];	                    // ACK id
+  ackPacket[8] = 0;		                    // CK_A
+  ackPacket[9] = 0;		                    // CK_B
 
-  // Calculate the checksums
-  for (uint8_t i=2; i<8; i++) {
+  for (uint8_t i=2; i<8; i++) {                     // Calculate the checksums
     ackPacket[8] = ackPacket[8] + ackPacket[i];
     ackPacket[9] = ackPacket[9] + ackPacket[8];
   }
   while (1) {
-
-    // Test for success
-    if (ackByteID > 9) {
-
-      // All packets in order!
-      Serial.println(" (SUCCESS!)");
+    if (ackByteID > 9) {                            // Test for success
       return true;
     }
-
-    // Timeout if no valid response in 3 seconds
-    if (millis() - startTime > 3000) { 
-      Serial.println(" (FAILED!)");
+    if (millis() - startTime > 3000) {              // Timeout if no valid response in 3 seconds 
       return false;
     }
-
-    // Make sure data is available to read
-    if (ss.available()) {
+    if (ss.available()) {                           // Make sure data is available to read
       b = ss.read();
-
-      // Check that bytes arrive in sequence as per expected ACK packet
-      if (b == ackPacket[ackByteID]) { 
+      if (b == ackPacket[ackByteID]) {              // Check that bytes arrive in sequence as per expected ACK packet 
         ackByteID++;
-        Serial.print(b, HEX);
       } 
       else {
-
-        // Reset and look again, invalid order
-        ackByteID = 0;
+        ackByteID = 0;                              // Reset and look again, invalid order
       }
     }
   }
 }
-
-//CRC16 checksum
-uint16_t gps_CRC16_checksum (char *string)
+                                                      
+uint16_t gps_CRC16_checksum (char *string)          // CRC16 checksum function
 {
   size_t i;
   uint16_t crc;
   uint8_t c;
-
-  crc = 0xFFFF;
-
-  // Calculate checksum ignoring the first two $s
-  for (i = 2; i < strlen(string); i++)
+  crc = 0xFFFF;                                                  
+  for (i = 2; i < strlen(string); i++)              // Calculate checksum ignoring the first two $s
   {
     c = string[i];
     crc = _crc_xmodem_update (crc, c);
   }
-
   return crc;
 }
 
+void rtty_txstring (char *string) {                 // Transmit a string, one char at a time		
+  int i;					    // Define disposable integer counter variable
+  for (i = 0; i < strlen(string); i++)	            // Iterate over the string array
+  {
+    rtty_txbyte(string[i]);			    // Pass each element of the string array to rtty_txbyte
+  }
+}
 
+void rtty_txbyte (char c)                           // Convert each character byte to bits
+{
+  int i;                                            // Define disposible integer counter variable
+  rtty_txbit (0); 				    // Start bit
+  for (i=0;i<ASCII;i++)				    // 7-bit ascii
+  {
+    if (c & 1) rtty_txbit(1); 		            // Starting with least significant bit, if bit=1 then transmit MARK 
+    else rtty_txbit(0);			            // If bit=0 then transmit SPACE
+    c = c >> 1;                                     // Shift along to the next bit
+  }
+  rtty_txbit (1);                                   // Stop bit
+  rtty_txbit (1);                                   // Stop bit                 
+}
 
+void rtty_txbit (int bit)                           // Transmit individual bits
+{
+  digitalWrite(RADIO_MARK_PIN,(bit>0)?HIGH:LOW);
+  digitalWrite(RADIO_SPACE_PIN,(bit>0)?LOW:HIGH);
+  delay(INTER_BIT_DELAY);                           // 50 bits per second i.e. baudrate
+}
