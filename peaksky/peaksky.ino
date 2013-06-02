156187faaadcf53a70b3361b6a6dda8c062fa1a9
John Tanner <j.d.tanner@gmail.com>
2013-06-02 14:06:05 +0100

    A few changes...
    I canged a few pins so that I can integrate an sd-card into the tracker
    to log the RTTY packets.

--- peaksky/peaksky.ino
+++ peaksky/peaksky.ino
@@ -11,19 +11,19 @@
 #include <Wire.h>
 #include <BMP085.h> //Using library from https://code.google.com/p/bmp085driver/
 
-//Make definitions
-#define ENABLE_RTTY 12 //This allows the arduino to pull the NTX2 EN pin high, which enables the radio module
+//State definitions
+#define ENABLE_RTTY 11 //This allows the arduino to pull the NTX2 EN pin high, which enables the radio module
 #define ASCII 7
 #define BAUD 50
 #define INTER_BIT_DELAY (1000/BAUD)
 #define PWM_PIN 9
-#define ONE_WIRE_BUS 6 //OneWire sensors will be attached to pin 4 of the Arduino
+#define ONE_WIRE_BUS 10 //OneWire sensors will be attached to pin 6 of the Arduino
 #define TEMPERATURE_PRECISION 12 //12-bit precision for OneWire sensors i.e. 2dp
 
 //Define some variables to hold GPS data
 unsigned long date, time, age;
 int hour, minute, second, numberOfSatellites, iteration = 1, transmitCheck;
-long int gpsAltitude, Pressure;
+long int gpsAltitude, bmpPressure;
 char latitudeBuffer[16], longitudeBuffer[16], timeBuffer[] = "00:00:00", transmitBuffer[128], insideTempBuffer[16], outsideTempBuffer[16];
 float floatLatitude, floatLongitude, outsideTemp, insideTemp;
 
@@ -43,7 +43,7 @@ DeviceAddress insideThermometer, outsideThermometer;
 BMP085 dps = BMP085();
 
 //Create new software serial object and define the pins on which it will work Rx=2, Tx=3
-SoftwareSerial ss(2,3);
+SoftwareSerial ss(12,13);
 
 //Setup function of Arduino
 void setup() {
@@ -53,7 +53,7 @@ void setup() {
   //Set up pin to enable radio, PWM pin, and PWM frequency
   pinMode(ENABLE_RTTY,OUTPUT); 
   pinMode(PWM_PIN, OUTPUT);
-  TCCR1B = TCCR1B & 0b11111000 | 0x01; //Increase the frequency of PWM pin 9 to allow for PWM control of NTX2
+  TCCR1B = TCCR1B & 0b11111000 | 0x01; //Increase the frequency of PWM pin 9 to 31250Hz to allow for PWM control of NTX2
 
   //Start up OneWire sensors, discover their addresses, and set their precision
   sensors.begin();
@@ -125,8 +125,10 @@ void loop() {
       second = second / 100;
 
       //Used for debugging only.
-      Serial.println("Waiting for satellite lock.");      
+      Serial.println("Waiting for satellite lock."); 
+      delay(1500);     
 
+      //Used for debugging only.
       if (age == TinyGPS::GPS_INVALID_AGE)
         Serial.println("No fix detected");
       else if (age > 5000)
@@ -163,15 +165,15 @@ void loop() {
         dtostrf(insideTemp, 6, 2, insideTempBuffer);
 
         //Request pressure in Pascals from BMP085
-        dps.getPressure(&Pressure);
+        dps.getPressure(&bmpPressure);
 
         //Construct the transmit buffer
-        sprintf(transmitBuffer, "$$PEAKSKY,%d,%02d:%02d:%02d,%s,%s,%ld,%d,%s,%s,%ld", iteration, hour, minute, second, latitudeBuffer, longitudeBuffer, gpsAltitude, numberOfSatellites, outsideTempBuffer, insideTempBuffer, Pressure);
+        sprintf(transmitBuffer, "$$PEAKSKY,%d,%02d:%02d:%02d,%s,%s,%ld,%d,%s,%s,%ld", iteration, hour, minute, second, latitudeBuffer, longitudeBuffer, gpsAltitude, numberOfSatellites, outsideTempBuffer, insideTempBuffer, bmpPressure);
 
         //Append the CRC16 checksum to the end of the transmit buffer
         sprintf(transmitBuffer, "%s*%04X\n", transmitBuffer, gps_CRC16_checksum(transmitBuffer));
 
-        //Debug purposes only.
+        //Used for debugging only.
         Serial.println(transmitBuffer);
 
         //Pass the transmit buffer to the RTTY function
